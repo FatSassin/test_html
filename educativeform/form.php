627bebd2831b2c0c2d4783b1efb13b00c7e1811c
Hubert Talwik (Uczeń) <htalwik379@technikumkreatywne.pl>
2023-10-05 09:35:21 +0200

    database working thx holo

---
+++ educativeform/form.php
@@ -0,0 +1,52 @@
+<?php
+    // getting all values from the HTML form
+    if(isset($_POST['submit']))
+    {
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+    }
+
+    // database details
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "bazadanychhtmltest";
+
+    // creating a connection
+    $con = mysqli_connect($host, $username, $password, $dbname);
+
+    // to ensure that the connection is made
+    if (!$con)
+    {
+        die("Connection failed!" . mysqli_connect_error());
+    }
+
+    // using sql to create a data entry query
+    $sql = "INSERT INTO tabela2 (id, fname, lname, email) VALUES ('0', '$fname', '$lname', '$email')";
+  
+    // send query to the database to add values and confirm if successful
+    $rs = mysqli_query($con, $sql);
+    if($rs)
+    {
+        echo "Entries added!";
+    }
+  
+    // close connection
+    mysqli_close($con);
+
+
+
+
+	/*
+	$db = mysqli_connect("localhost","root","","bazadanychhtmltest");
+    
+    $fishID = $_GET['id'];
+    $sql1 = "SELECT fish.*, fishdesc_".$selectedLanguage.".*
+         FROM fish
+         INNER JOIN fishdesc_".$selectedLanguage." ON fish.fishID = fishdesc_".$selectedLanguage.".fishID
+         WHERE fish.fishID = $fishID";
+    $result = $db->query($sql1);
+    $row = $result->fetch_assoc();
+	*/
+?>
